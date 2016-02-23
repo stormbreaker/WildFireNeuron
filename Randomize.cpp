ae122f3502e8cbe4dd6539c47a04340d7c8f7c53
Haberling <1973266@linux07.mcs.sdsmt.edu>
2016-02-23 15:36:44 -0700

    Added create order function

--- Randomize.cpp
+++ Randomize.cpp
@@ -8,6 +8,25 @@ Description:
 ******************************************************************************/
 #include "Randomize.h"
 
+vector<int> create_order( int number_of_records )
+{
+    vector<int> in_order;
+    vector<int> to_return;
+    for( int x = 0; x < number_of_records; x++ )
+    {
+         in_order.push_back(x);
+    }
+    for( int x = number_of_records; x > 0; x -- )
+    {
+        int pos = rand()%x;
+        to_return.push_back(in_order.at(pos));
+        int added = in_order.at(pos);
+        in_order.at(pos) = in_order.at(x-1);
+        in_order.at(x-1) = added;
+    }
+	return to_return;
+}
+
 void randomize( vector< vector<double> > & data )
 {
 	// seeding for random number generator
--- Randomize.h
+++ Randomize.h
@@ -12,8 +12,8 @@ Description:
 #include <stdlib.h>
 #include <time.h>
 #include <vector>
-
+#include <list>
 using namespace std;
 
 void randomize( vector< vector<double> > & data );
-
+vector<int> create_order( int number_of_records );
